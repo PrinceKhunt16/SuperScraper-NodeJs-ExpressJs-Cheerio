49331ac69cbb852508c03f12a4e396fffc20c7d9
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-01-25 18:04:59 +0530

    Collect all links of gfg page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cheerio = require('cheerio')
 const axios = require('axios')
 const app = express()
-const URL = 'https://www.theguardian.com/uk'
+const URL = 'https://www.geeksforgeeks.org/data-structures/'
 const PORT = 3000 
 
 app.get('/', function(req, res){
@@ -10,15 +10,23 @@ app.get('/', function(req, res){
   .then((response) => {
     const html = response.data
     const $ = cheerio.load(html)
-    const data = []
-    
-    $('.fc-item__title', html).each(function(){
-      const title = $(this).text()
-      const url = $(this).find('a').attr('href')
-      data.push({title, url})
+    const dsaTopicsLinks = []
+     
+    $('#post-125686', html).each(function(){
+      const topicLinks = []
+ 
+      $(this).find('ol').each(function() {
+        $(this).children(function() {
+          const title = $(this).text()
+          const url = $(this).find('a').attr('href')
+          topicLinks.push({title, url})
+        })
+
+        dsaTopicsLinks.push(topicLinks)
+      })
     })
     
-    res.json(data)
+    res.json(dsaTopicsLinks)
   })
   .catch((err) => {
     console.log(err)
